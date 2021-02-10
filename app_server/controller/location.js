2e2522c774ee377132f6bba10354333c1c339a82
Charu Kush <charu.kush@gmail.com>
2021-02-10 16:55:49 -0500

    Data from controller

--- app_server/controller/location.js
+++ app_server/controller/location.js
@@ -23,10 +23,62 @@ const homeList = (req,res) => {
 }
 
 const locatiobInfo = (req,res) => {
-    res.render('location-info', {title: "Location Info Detailed page"});
+    res.render('location-info', {
+        title: "Dunkin",
+         pageHeader:{
+             title: 'Loc8r',
+         },
+         sidebar: {
+             contex:' Contex(Side bar) on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+             callToAction: 'If(Side bar call on Action text) you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+         },
+         location: {
+             name: 'Dunkin',
+             address: '125 Street, Herndon',
+             rating: 5,
+             facilities: ['Hot Drinks', 'Food', 'Premium Wifi'],
+             cords: {lat: 51.455041, lng: -0.9690884},
+             openingTimes: [
+                 {
+                    days: 'Monday - Friday',
+                    opening: '7:00am',
+                    closing: '7:00pm',
+                    closed: false
+                 },
+                 {
+                    days: 'Saturday',
+                    opening: '8:00am',
+                    closing: '5:00pm',
+                    closed: false
+                 },
+                 {
+                    days: 'Sunday',
+                    closed: true
+                 }
+             ],
+             reviews:[
+                 {
+                    author: 'Simon Holmes',
+                    rating: 5,
+                    timestamp: '16 July 2013',
+                    reviewText: 'What a great place. I can\'t say enough good things about it.'
+                 },
+                 {
+                    author: 'Charlie Chaplin',
+                    rating: 3,
+                    timestamp: '16 June 2013',
+                    reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.' 
+                 }
+             ]
+         }
+    });
 }
 
 const addReview = (req,res) => {
-    res.render('location-review-form', {title: "Add Review"});
+    res.render('location-review-form', 
+    {
+        title: "Review Location",
+        pageHeader: { title: 'Review Dunkins'}
+    });
 }
 module.exports = {homeList, locatiobInfo, addReview};
