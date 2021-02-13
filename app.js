8b857e7d0fef8a7ff4105ff9ab8957ec825ba4f9
Charu Kush <charu.kush@gmail.com>
2021-02-13 17:38:02 -0500

    Connected MangoDb to Application

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
-
+require('./app_server/models/db');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 
---
+++ app_server/models/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dbURI = process.env.mongoUrl;
+mongoose.connect(dbURI, {useNewUrlParser: true});
+
+mongoose.connection.on('connected', () =>{
+    console.log(`Mongoose connected to ${dbURI}`);
+});
+
+mongoose.connection.on('error', err =>{
+    console.log('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () =>{
+    console.log('Mongoose disconnected');
+});
+
+require('./locations');
