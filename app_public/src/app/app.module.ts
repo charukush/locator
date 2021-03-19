e040cf2bdf23954328b5964a1d05bc357ffb87f0
Charu Kush <charu.kush@gmail.com>
2021-03-18 20:28:35 -0400

    create resuable componeny for site

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -4,16 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeListComponent } from './home-list/home-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FrameworkComponent } from './framework/framework.component';
+import { AboutComponent } from './about/about.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HtmlLineBreaksPipe } from './html-line-breaks.pipe';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeListComponent
+    HomeListComponent,
+    FrameworkComponent,
+    AboutComponent,
+    HomepageComponent,
+    PageHeaderComponent,
+    SidebarComponent,
+    HtmlLineBreaksPipe
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path:'',
+        component: HomepageComponent
+      },
+      {
+        path:'about',
+        component: AboutComponent
+      }
+    ])
   ],
   providers: [],
-  bootstrap: [HomeListComponent]
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule { }
---
+++ app_public/src/app/homepage/homepage.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  public pageConent = {
+    header: {
+      title:'Loc8r',
+      strapline: 'Find places to work with wifi near you!'
+    },
+    sidebar: 'This is a Side bar Locator(Controller) helps you find palces to work out and about.'
+  };
+
+}
